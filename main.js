44ed9c873cb0ec8d0e39fcbebb858f4cf901fde5
rahat <rahat@a4aero.com>
2025-03-26 15:00:13 +0600

    var,let and const

---
+++ main.js
@@ -0,0 +1,26 @@
+// var
+var firstNumber;
+
+console.log(firstNumber);
+
+firstNumber = 10;
+
+console.log(firstNumber);
+
+var firstNumber = 20;
+
+console.log(firstNumber);
+
+//let
+let secondNumber = 25;
+
+console.log(secondNumber);
+
+secondNumber = 30;
+
+console.log(secondNumber);
+
+//const
+
+const thirdNumber = 40;
+console.log(thirdNumber);
