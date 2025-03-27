f762a8f6456b81b9463de27204195693f00c3f74
rahat <rahat@a4aero.com>
2025-03-27 11:55:14 +0600

    operators

--- main.js
+++ main.js
@@ -24,3 +24,38 @@ console.log(secondNumber);
 
 const thirdNumber = 40;
 console.log(thirdNumber);
+
+
+//Mathematics operators
+
+const now = 2025;
+
+const myAge = now - 1994;
+const wifeAge = now - 1996;
+
+console.log(myAge , wifeAge, myAge * 2, wifeAge / 10, 2**4);
+
+//assignment Operator
+
+let firstName = "Raqibul";
+let lastName = "Hasan";
+
+console.log(firstName + lastName);
+console.log(firstName + ' ' + lastName);
+
+let age = 25;
+age += 25;
+age -= 25;
+age *= 25;
+age /= 25;
+age++;
+age--;
+age--;
+console.log(age);
+
+
+//comperison operator
+
+console.log(myAge > wifeAge);
+console.log(wifeAge >= 30);
+console.log(now-1994 > now-1996)
